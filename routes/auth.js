749b8ec69a9ac78cce778bb99bd74ce743474837
Lee <jrmun123@gmail.com>
2023-06-20 15:47:01 +0900

    회원 가입

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../schemas/user.js");
+const jwt = require("jsonwebtoken");
+
+router.post("/auth", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || user.password !== password) {
+    res.status(400).json({ errorMessage: "로그인 실패" });
+    return;
+  }
+
+  const token = jwt.sign({ userId: user.userId }, "customized-secret-key");
+  res.cookie("Authorization", `Bearer ${token}`);
+  res.status(200).json({ token });
+});
+
+module.exports = router;
---
+++ schemas/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchemas = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchemas.virtual("userId").get(function () {
+  return this._id.toHexString();
+});
+
+userSchemas.set("toJSON", {
+  virtuals: true,
+});
+module.exports = mongoose.model("user", userSchemas);
